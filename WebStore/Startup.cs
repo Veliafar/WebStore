344a5176f631b045aeb52b5d9bf1243b26999aeb
Veliafar <denis.seo@nxt.ru>
2021-09-20 14:12:46 +0300

    Подключение конфигурации в StartUp

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -12,12 +12,12 @@ namespace WebStore
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public IConfiguration Configuration { get; set; }
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
