6bbdb74e249ecd7fe64ab10fb4b0f01a5ff167a2
Veliafar <denis.seo@nxt.ru>
2021-09-20 14:45:59 +0300

    first controller

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() // http://localhost:500/Home/Index
+        {
+            return Content("Hello from first Controller");
+        }
+
+        public IActionResult SecondAction(string id)
+        {
+            return Content($"Second action with parametr {id}");
+        }
+    }
+}
