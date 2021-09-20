c449bbba848e345e9a995a32e3e484dd134242f8
Veliafar <denis.seo@nxt.ru>
2021-09-20 15:14:57 +0300

    add View Emplyee

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+
+
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 19 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 34 },
+        };
+
         public IActionResult Index() // http://localhost:500/Home/Index
         {
             return View();
@@ -16,6 +23,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction(string id)
         {
             return Content($"Second action with parametr {id}");
+        }
+
+        public IActionResult Employees() // http://localhost:500/Home/Employees
+        {
+            return View(__Employees);
         }
     }
 }
