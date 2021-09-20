3a12b91a5bcb50bc63284ac7cd8562981b53d2bd
Veliafar <denis.seo@nxt.ru>
2021-09-20 15:18:08 +0300

    add RazorRuntime package

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -19,7 +19,9 @@ namespace WebStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services
+                .AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
