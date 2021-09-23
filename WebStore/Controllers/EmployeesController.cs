818d79a3a22db778a29665a273d7819ac31a6833
Veliafar <denis.seo@nxt.ru>
2021-09-23 20:21:35 +0300

    Move Employees to standalone controller

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27, Phone = "+79997778881" },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 19, Phone = "+79997778882" },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 34, Phone = "+79997778883" },
+        };
+
+        public IActionResult Index() => View(__Employees);// http://localhost:500/Home/Employees        
+
+        public IActionResult Employee(int id)
+        {
+            var employee = __Employees.Find(x => x.Id == id);
+            return View(employee);
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using WebStore.Models;
 
 namespace WebStore.Controllers
 {
@@ -8,32 +6,13 @@ namespace WebStore.Controllers
     {
 
 
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27, Phone = "+79997778881" },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 19, Phone = "+79997778882" },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 34, Phone = "+79997778883" },
-        };
+        
 
-        public IActionResult Index() // http://localhost:500/Home/Index
-        {
-            return View();
-        }
+        public IActionResult Index() => View();// http://localhost:500/Home/Index
 
         public IActionResult SecondAction(string id)
         {
             return Content($"Second action with parametr {id}");
-        }
-
-        public IActionResult Employees() // http://localhost:500/Home/Employees
-        {
-            return View(__Employees);
-        }
-
-        public IActionResult Employee(int id)
-        {
-            var employee = __Employees.Find(x => x.Id == id);
-            return View(employee);
-        }
+        }         
     }
 }
