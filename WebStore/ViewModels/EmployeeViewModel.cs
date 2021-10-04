c8cb22f364b88c1cc74ea5fc74d15839f2828acf
Veliafar <denis.seo@nxt.ru>
2021-10-04 17:40:19 +0300

    visual data annotation of view model

--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +9,22 @@ namespace WebStore.ViewModels
 {
     public class EmployeeViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+
+        [Display(Name = "Имя")]
         public string Name { get; set; }
+
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
+
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
+
+        [Display(Name = "Телефон")]
         public string Phone { get; set; }
     }
 }
