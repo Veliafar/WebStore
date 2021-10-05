ffdfb3196d68160e35c73613276a555af667458a
Veliafar <denis.seo@nxt.ru>
2021-10-05 07:07:21 +0300

    Prepare visual components

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Components
+{
+    //[ViewComponent(Name = "BrandsView")]
+    public class BrandsViewComponent : ViewComponent
+    {
+        //public async Task<IViewComponentResult> InvokeAsync() => View();
+        public IViewComponentResult Invoke() => View();
+        
+
+
+    }
+}
