717e6c1f3e140694f6a63d9f710a9a5a07d6358e
Veliafar <denis.seo@nxt.ru>
2021-10-27 11:31:03 +0300

    add Identity DB init data logic

--- WebStore/Data/WebStoreDbInitializer.cs
+++ WebStore/Data/WebStoreDbInitializer.cs
@@ -8,19 +8,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Data
 {
     public class WebStoreDbInitializer
     {
         private readonly WebStoreDB _db;
+        private readonly UserManager<User> _UserManager;
+        private readonly RoleManager<Role> _RoleManager;
         private readonly ILogger<WebStoreDbInitializer> _Logger;
 
-        public WebStoreDbInitializer(WebStoreDB db,
-            ILogger<WebStoreDbInitializer> Logger
-            )
+        public WebStoreDbInitializer(
+            WebStoreDB db,
+            UserManager<User> UserManager,
+            RoleManager<Role> RoleManager,
+            ILogger<WebStoreDbInitializer> Logger)
         {
             _db = db;
+            _UserManager = UserManager;
+            _RoleManager = RoleManager;
             _Logger = Logger;
         }
 
@@ -51,16 +58,16 @@ namespace WebStore.Data
                 throw;
             }
 
-            //try
-            //{
-            //    await InitializeIdentityAsync();
-            //}
-            //catch (Exception e)
-            //{
+            try
+            {
+                await InitializeIdentityAsync();
+            }
+            catch (Exception e)
+            {
 
-            //    _Logger.LogError(e, "Identity system init error");
-            //    throw;
-            //}
+                _Logger.LogError(e, "Identity system init error");
+                throw;
+            }
         }
 
         private async Task InitializeProductsAsync()
@@ -101,9 +108,64 @@ namespace WebStore.Data
 
         }
 
-        //private async Task InitializeIdentityAsync()
-        //{
+        private async Task InitializeIdentityAsync()
+        {
+            _Logger.LogInformation("Identity system init");
+            var timer = Stopwatch.StartNew();
 
-        //}
+            //if(!await _RoleManager.RoleExistsAsync(Role.Administrators))
+            //{
+            //    await _RoleManager.CreateAsync(new Role { Name = Role.Administrators });
+            //}
+
+
+            async Task CheckRole(string RoleName)
+            {
+                if (await _RoleManager.RoleExistsAsync(RoleName))
+                {
+                    _Logger.LogInformation("Role {0} is exist", RoleName);
+                }
+                else
+                {
+                    _Logger.LogInformation("Role {0} is Not exist", RoleName);
+                    await _RoleManager.CreateAsync(new Role { Name = RoleName });
+                    _Logger.LogInformation("Role {0} successfuly created", RoleName);
+                }
+            }
+
+            await CheckRole(Role.Administrators);
+            await CheckRole(Role.Users);
+
+            if ( await _UserManager.FindByNameAsync(User.Administrator) is null)
+            {
+                _Logger.LogInformation("User {0} is Not exist", User.Administrator);
+
+                var admin = new User
+                {
+                    UserName = User.Administrator
+                };
+
+                var creation_result = await _UserManager.CreateAsync(admin, User.DefaultAdminPass);
+                if (creation_result.Succeeded)
+                {
+                    _Logger.LogInformation("User {0} successfuly created", User.Administrator);
+
+                    await _UserManager.AddToRoleAsync(admin, Role.Administrators);
+
+                    _Logger.LogInformation("User {0} successfuly get role {1}"
+                        , User.Administrator
+                        , Role.Administrators);
+                } else
+                {
+                    var errors = creation_result.Errors.Select(err => err.Description).ToArray();
+                    _Logger.LogInformation("Administrator account is Not created! Error: {0}"
+                        , string.Join(", ", errors));
+
+                    throw new InvalidOperationException($"Impossible to create Administator {string.Join(", ", errors)}");
+                }
+
+                _Logger.LogInformation("Identity system data added successfuly to DB for {0} ms", timer.Elapsed.TotalMilliseconds);
+            }
+        }
     }
 }
